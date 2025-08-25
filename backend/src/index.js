9c2fe089d60f1713f9adfc6c56f148a1afd5bf4e
CurryDevs <currydevs.co@gmail.com>
2025-08-25 10:28:38 +0530

    prodcution

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173","https://chat-application-two-silk-96.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-application-two-silk-96.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
